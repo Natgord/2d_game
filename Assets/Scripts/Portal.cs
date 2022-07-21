f827ccb8476cd072797af0d6ccb652f972b02780
Nicolas St-Jean <nicolas.st-jean@usherbrooke.ca>
2022-07-20 23:45:14 -0400

    Modification du Portal. Un Portal Slave et master. Slave prend la couleur du master donc congig juste un portal pour la couleur. teleport seulement si la bonne couleur

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,22 +4,65 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    private Transform player_destination;
 
     public GameObject Destination;
     public float distance;
 
+    private Transform player_destination;
+    private Collider2D m_collision;
+
     private void Start()
     {
-        player_destination = Destination.GetComponent<Transform>();
+        CheckPriority();
+        CheckDestination();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Vector2.Distance(transform.position, collision.transform.position) > distance) 
+        m_collision = collision;
+        CheckCollision();
+    }
+
+    private void CheckPriority() 
+    {
+        if (gameObject.CompareTag("Portal_Slave")) 
         {
-            collision.transform.position = new Vector2(player_destination.position.x, player_destination.position.y);
-            
+            gameObject.GetComponent<SpriteRenderer>().color = Destination.GetComponent<SpriteRenderer>().color;
         }
     }
+
+    private void CheckDestination()
+    {
+        player_destination = player_destination = Destination.GetComponent<Transform>();
+    }
+
+    private void CheckCollision() 
+    {
+        if (m_collision.gameObject.CompareTag("Player"))
+        {
+            CheckColor();
+            //GoToDestination();
+        }
+        else if (m_collision.gameObject.CompareTag("Projectil"))
+        {
+            CheckColor();
+        }
+    }
+
+    private void CheckColor()
+    {
+        if (m_collision.gameObject.GetComponent<SpriteRenderer>().color == gameObject.GetComponent<SpriteRenderer>().color)
+        {          
+            GoToDestination();
+        }
+    }
+
+    private void GoToDestination() 
+    {
+        if (Vector2.Distance(transform.position, m_collision.transform.position) > distance)
+        {
+            m_collision.transform.position = new Vector2(player_destination.position.x, player_destination.position.y);
+        }
+    }
+
 }
