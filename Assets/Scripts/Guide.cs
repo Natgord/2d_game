3538665e7bc61039621538bcb0a29d444a29223b
Natgord <natgourdeau@gmail.com>
2022-07-24 23:00:51 +0200

    Add portal sprite and animation

--- Assets/Scripts/Guide.cs
+++ Assets/Scripts/Guide.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Guide : MonoBehaviour
 {
 
+    public Text whatToDoText;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +32,30 @@ public class Guide : MonoBehaviour
         {
             GetComponent<SpriteRenderer>().flipX = true;
             GameObject.Find("GuideEyes").GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        // Get the tag of the collsion
+        string collisionTag = other.gameObject.tag;
+
+        if (collisionTag == "Player")
+        {
+            whatToDoText.enabled = true;
+            whatToDoText.text = "Press B to interact with the Guide";
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Get the tag of the collsion
+        string collisionTag = other.gameObject.tag;
+
+        if (collisionTag == "Player")
+        {
+            whatToDoText.text = "";
+            whatToDoText.enabled = false;
         }
     }
 }
